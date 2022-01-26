ffd2d76ecc4b26a50dc91e3ba8d26311393b50f0
Belial <belyal4ik@gmail.com>
2022-01-26 16:43:30 +0300

    fix bags end add localStorage

--- script/script.js
+++ script/script.js
@@ -3,18 +3,86 @@ const shopWeight = document.querySelector("#shop-weight")
 const shopPosition = document.querySelector("#shop-position")
 const addNewListButton = document.querySelector("#add-button")
 const container = document.querySelector(".container")
+const clearBtn = document.querySelector("#clearBtn")
 
-const count = document.querySelector("#count")
-const countOfWeight = document.querySelector("#count-of-weight")
-const countOfPositions = document.querySelector("#count-of-positions")
+const countSpan = document.querySelector("#count")
+const countOfWeightSpan = document.querySelector("#count-of-weight")
+const countOfPositionsSpan = document.querySelector("#count-of-positions")
 
-let c12 = 0
+let x = 0
 let w = 0
 let p = 0
 
-count.innerText = c12
-countOfWeight.innerText = w
-countOfPositions.innerText = p
+count.innerText = x
+countOfWeightSpan.innerText = w
+countOfPositionsSpan.innerText = p
+
+let arrayData = []
+
+let objectData = {}
+
+let getStoreData = JSON.parse(localStorage.getItem("list"))
+
+function loadFromLocalStorage() {
+  if (getStoreData) {
+    getStoreData.forEach(({totalCount,nameOfMagazine,countOfWeight,countOfPositions}) => {
+      const div = document.createElement("div")
+      const span1 = document.createElement("span")
+      const span2 = document.createElement("span")
+      const span3 = document.createElement("span")
+      const button = document.createElement("button")
+
+      span1.className = "column"
+      span2.className = "column"
+      span3.className = "column"
+      div.className = "d-flex"
+      button.className = "button"
+      button.innerText = "удалить"
+      container.appendChild(div)
+      div.append(span1)
+      div.append(span2)
+      div.append(span3)
+      div.append(button)
+      
+      span1.innerText += nameOfMagazine
+      span2.innerText += countOfWeight
+      span3.innerText += countOfPositions
+      console.dir(span1)
+      console.dir(span2)
+      console.dir(span3)
+
+      w += +span2.outerText
+      p += +span3.outerText
+
+      // console.log(countOfWeight);
+      // console.log(countOfPositions);
+
+      countSpan.innerText = getStoreData.length
+      countOfWeightSpan.innerText = w
+      countOfPositionsSpan.innerText = p      
+
+      button.addEventListener("click", () => {
+        const outerTextSpan2 = span2.outerText;
+        const outerTextSpan3 = span3.outerText;        
+        
+        w -= +outerTextSpan2
+        p -= +outerTextSpan3
+
+        countSpan.innerText = getStoreData.length -= 1
+        countOfWeightSpan.innerText = w
+        countOfPositionsSpan.innerText = p
+
+        container.removeChild(div)
+
+        // getStoreData.filter((el,id) => {
+        //   if(el.id === id) {
+            
+        //   }
+        // })
+      })
+    })
+  }  
+}
 
 addNewListButton.addEventListener('click', (e) => {
   if (shopWeight.value === '' && shopPosition.value === '') {
@@ -33,26 +101,30 @@ function createDeleteElement(valueShopName,valueWeight,valuePositions) {
   const span2 = document.createElement("span")
   const span3 = document.createElement("span")
   const button = document.createElement("button")
-  console.log(c12);
-
-  c12 += 1
+  // console.log(getStoreData.length);
+// 
+  // let aaa = getStoreData.length
+  let newX = getStoreData == null ? x += 1 : getStoreData.length + 1
   w += +valueWeight
   p += +valuePositions
 
-  count.innerText = c12
-  countOfWeight.innerText = w
-  countOfPositions.innerText = p
+  countSpan.innerText = newX
+  countOfWeightSpan.innerText = w
+  countOfPositionsSpan.innerText = p
 
-  console.log(valueShopName);
-  console.log(valueWeight);
-  console.log(valuePositions);
+  objectData = {
+    totalCount:  newX,
+    nameOfMagazine: valueShopName.toUpperCase(),
+    countOfWeight: +valueWeight,
+    countOfPositions: +valuePositions
+  }
 
   span1.className = "column"
   span2.className = "column"
   span3.className = "column"
-  div.className = "row"
+  div.className = "d-flex"
   button.className = "button"
-  button.innerText = "delete"
+  button.innerText = "удалить"
   
   span1.innerText = valueShopName
   span2.innerText = valueWeight
@@ -63,5 +135,31 @@ function createDeleteElement(valueShopName,valueWeight,valuePositions) {
   div.append(span2)
   div.append(span3)
   div.append(button)
+  
+  getStoreData === null ? arrayData.push(objectData) : getStoreData.push(objectData)
+  
+  let store = getStoreData === null ? JSON.stringify(arrayData) : JSON.stringify(getStoreData)
+  localStorage.setItem("list", store)
+
+  button.addEventListener("click", () => {
+    const outerTextSpan2 = span2.outerText;
+    const outerTextSpan3 = span3.outerText;
+    x--
+    w -= +outerTextSpan2
+    p -= +outerTextSpan3
+
+    countSpan.innerText = x
+    countOfWeightSpan.innerText = w
+    countOfPositionsSpan.innerText = p
+
+    container.removeChild(div)
+
+  })
 }
 
+clearBtn.addEventListener("click", () => {
+  localStorage.clear()
+  location.reload()
+})
+
+loadFromLocalStorage()
